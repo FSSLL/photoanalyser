3116a7318be225ff8d422aefe5a86d9ba2e5c4ca
FSSLL <almurekhe@gmail.com>
2026-04-04 01:49:00 +0300

    release: v1.0.0 build 20

--- artifacts/photo-finder/app.config.js
+++ artifacts/photo-finder/app.config.js
@@ -16,7 +16,7 @@ module.exports = {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.fssll.photofinder",
-      buildNumber: "22",
+      buildNumber: "20",
       deploymentTarget: "17.0",
       infoPlist: {
         NSPhotoLibraryUsageDescription:
