9526016ca3333a84e89d2c7f7d5b579671a94301
FSSLL <almurekhe@gmail.com>
2026-04-03 03:23:54 +0300

    build: bump to 1.0.0 (13)

--- artifacts/photo-finder/app.config.js
+++ artifacts/photo-finder/app.config.js
@@ -16,7 +16,7 @@ module.exports = {
     ios: {
       supportsTablet: false,
       bundleIdentifier: "com.fssll.photofinder",
-      buildNumber: "12",
+      buildNumber: "13",
       deploymentTarget: "17.0",
       infoPlist: {
         NSPhotoLibraryUsageDescription:
